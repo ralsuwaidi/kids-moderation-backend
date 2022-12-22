50251b5b310686901774f017f0f9f6cf1c9975ae
rashed <x.suwaidi@gmail.com>
2022-12-22 10:57:00 +0400

    add help text

--- content_moderation/moderation/models.py
+++ content_moderation/moderation/models.py
@@ -6,19 +6,19 @@ from autoslug import AutoSlugField
 class Movie(models.Model):
     title = models.CharField("Movie title", max_length=150)
     slug = AutoSlugField(populate_from='title') # auto field
-    poster = models.URLField("Movie Poster")
-    summary = QuillField()
-    violence = QuillField()
-    violence_rating = models.IntegerField()
-    language = QuillField()
-    language_rating = models.IntegerField()
-    inappropriate = QuillField()
-    inappropriate_rating = models.IntegerField()
-    drug_usage = QuillField()
-    religious_imagery = QuillField()
-    year = models.IntegerField()
+    poster = models.URLField("Movie Poster", help_text="use a url like: https://www.criticsinc.com/photos/movieposters/l/lightyear.jpg")
+    summary = QuillField("Movie Summary")
+    violence = QuillField("Violence & Gore")
+    violence_rating = models.IntegerField("Violence & Gore Percentage", help_text="percentage out of 100 where 100 is the maximum")
+    language = QuillField("Language")
+    language_rating = models.IntegerField("Language Percentage", help_text="percentage out of 100 where 100 is the maximum")
+    inappropriate = QuillField("Inappropriate")
+    inappropriate_rating = models.IntegerField("Inappropriate Percentage", help_text="percentage out of 100 where 100 is the maximum")
+    drug_usage = QuillField("Drug Usage")
+    religious_imagery = QuillField("Religious Imagery")
+    year = models.IntegerField("Year of Publish")
     rating = models.CharField(help_text="movie rating (eg, PG, R Rated)", max_length=20)
-    duration = models.CharField(help_text="eg 2h 33m", max_length=10)
+    duration = models.CharField("Movie Duration", help_text="eg 2h 33m", max_length=10)
 
     def __str__(self):
         return self.title
